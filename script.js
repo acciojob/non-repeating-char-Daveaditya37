d551559a1c107760823756278d97f5f8af1e2f24
Daveaditya37 <160773920+Daveaditya37@users.noreply.github.com>
2024-08-31 15:04:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function firstNonRepeatedChar(str) {
   for (let i = 0; i < str.length; i++) {
-    let char = str[i];
-    if (str.indexOf(char) == i && str.indexOf(char, i + 1) == -1) {
-      return "c";
+    let c = str[i];
+    if (str.indexOf(c) == i && str.indexOf(c, i + 1) == -1) {
+      return c;
     }
   }
-  return "null";
-	
+  return null;
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
